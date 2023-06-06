da671ca7238c7c0c146d38fb2e688a9a01375c56
enginozturkk <128605311+enginozturkk@users.noreply.github.com>
2023-06-06 19:24:29 +0300

    Add files via upload

---
+++ js_uygulama/script.js
@@ -0,0 +1,45 @@
+// Soru 1
+
+let isim = "Engin";
+let yas = 27;
+let aktif ;
+aktif = (isim =="Engin");
+console.log(aktif);
+console.log(typeof isim);
+console.log(typeof yas);
+
+//Soru 2 
+let city = ["Aydın" , "İzmir" , "İstanbul","Ankara","Antalya"]
+console.log(city)
+sonuc = city.indexOf("İstanbul");
+city.push("Bursa")
+console.log(city.length);
+console.log(sonuc);
+
+//Soru 3
+
+let sayi1 = 15;
+let sayi2= 20;
+console.log(sayi1 + sayi2);
+console.log(sayi2 - sayi1);
+console.log(sayi1 * sayi2);
+console.log(sayi1 / sayi2);
+console.log(sayi2 % sayi1);
+
+//Soru 4
+let sayi3 = 12;
+let sayi4= 16;
+console.log(sayi3 == sayi4);
+console.log(sayi3 != sayi4);
+console.log(sayi3 < sayi4);
+
+//Soru 5
+let x = 40;
+let y = 30;
+let toplam;
+toplam = x + y;
+console.log(toplam);
+x = x + 2;
+y = y - 5;
+toplam1 = x+y;
+console.log(toplam1);
